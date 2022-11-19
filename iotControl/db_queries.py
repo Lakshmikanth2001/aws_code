8f56948f749ff50c5adb40463048580063b579e5
lakshmikanth ayyadevara <lakshmikanth2001@gmail.com>
2022-11-19 16:16:54 +0530

    sql string contains `\n`

--- iotControl/db_queries.py
+++ iotControl/db_queries.py
@@ -1,11 +1,17 @@
-from db_helper import Database
 from datetime import datetime
+
+def sql_formate(method):
+    def wrapper(*args, **kwargs):
+        sql_string = method(*args, **kwargs)
+        sql_string = sql_string.replace('\n', '')
+        return sql_string
+    return wrapper
 
 class DatabaseQueries:
     def __init__(self, device_id) -> None:
-        self.db_instance = Database()
         self.device_id = device_id
 
+    @sql_formate
     def get_power_suply_status(self) -> str:
         return f"""
         SELECT power_supply, clear_wifi, control_bits, timer_states
@@ -13,6 +19,7 @@ class DatabaseQueries:
         ON user_devices.id = device_control.owner_id
         WHERE `device_id` = '{self.device_id}';"""
 
+    @sql_formate
     def get_handshakes_timer_states(self) -> str:
         return f"""
         SELECT * FROM `device_timers`
@@ -21,6 +28,7 @@ class DatabaseQueries:
         WHERE `device_id` = '{self.device_id}';
         """
 
+    @sql_formate
     def update_wifi_status(self) -> str:
         return f"""
         UPDATE `user_devices`
@@ -29,12 +37,14 @@ class DatabaseQueries:
         SET `clear_wifi` = false
         WHERE `device_id` = '{self.device_id}';"""
 
+    @sql_formate
     def update_hardware_handshake_time(self, timezone) -> str:
         current_time = datetime.now(timezone)
         return f"""UPDATE `device_control`
         SET `handshake_time` = '{current_time.strftime("%Y-%m-%d %H:%M:%S")}'
         WHERE `device_id` = '{self.device_id}'"""
 
+    @sql_formate
     def update_after_timer_overflow(
         self, new_control_bits: str, new_timer_states: str
     ) -> str:
@@ -44,17 +54,20 @@ class DatabaseQueries:
         WHERE `device_id` = '{self.device_id}';
         """
 
+    @sql_formate
     def create_power_session(self, device_bit_id: str, trigger_time: str):
         return f"""
         INSERT INTO device_power_session(`device_bit_id`, `on_time`)
-        VALUES ({device_bit_id}, {trigger_time})
+        VALUES ('{device_bit_id}', '{trigger_time}')
         """
 
+    @sql_formate
     def complete_power_session(self, device_bit_id: str, trigger_time: str):
         return f"""
-        UPDATE `device_power_session` SET `off_time` = {trigger_time}
+        UPDATE `device_power_session` SET `off_time` = '{trigger_time}'
         WHERE `id` = (
             SELECT `id` FROM `device_power_session`
-            WHERE `device_bit_id` = {device_bit_id}
-            AND `off_time` IS NULL)
+            WHERE `device_bit_id` = '{device_bit_id}'
+            AND `off_time` IS NULL
+        )
         """
